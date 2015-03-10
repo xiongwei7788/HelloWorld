ecbab2e955da3e8a1387df62910b0e9cf61f8ed6
xiongwei7788 <1045947639@qq.com>
2015-03-10 16:47:10 +0800

    第二次commit 新建了类文件HelloGit.java

---
+++ HelloGit/src/com/day/test/HelloGit.java
@@ -0,0 +1,11 @@
+/*
+ *www.dyr.com
+ *Copyright(c) 2014 All Rights Reserved.
+ */
+package com.day.test;
+
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Git!!!!");
+	}
+}
