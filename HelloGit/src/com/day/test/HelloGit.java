f4ec65de7e794efe4e0680ef047e8651d2ad6b86
xiongwei7788 <1045947639@qq.com>
2015-03-10 17:17:33 +0800

    上传到了哟！！！！

--- HelloGit/src/com/day/test/HelloGit.java
+++ HelloGit/src/com/day/test/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
 		System.out.println("Hello Git!!!!");
 		System.out.println("增加了一条语句！！");
 		System.out.println("又增加一条语句！！");
+		System.out.println("我找到github服务器去看看！！");
 	}
 }
