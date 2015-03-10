75fdfbfbd0d9eb2728d8282fd6a1b93bd4550ad3
xiongwei7788 <1045947639@qq.com>
2015-03-10 16:51:41 +0800

    添加了一条语句

--- HelloGit/src/com/day/test/HelloGit.java
+++ HelloGit/src/com/day/test/HelloGit.java
@@ -7,5 +7,7 @@ package com.day.test;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!!!");
+		System.out.println("增加了一条语句！！");
+		System.out.println("又增加一条语句！！");
 	}
 }
